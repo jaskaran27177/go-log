f2809c0b2fbf5cefb9315df6d23bc250996f7dff
jaskaran27177 <jaskarandhanoa7@gmail.com>
2025-03-03 23:07:19 -0800

    update readme.md

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# go-log// dummy change
+# logger
+
+A simple Go logging package with customizable output writer support.
+
+## Installation
+
+To install this package, run:
+
+```bash
+go get github.com/jaskaran27177/go-log
+```
+
+## Usage
+
+```go
+package main
+
+import (
+	"github.com/jaskaran27177/go-log/logger"
+	"bytes"
+)
+
+func main() {
+	// Log using the default logger (writes to stdout)
+	logger.DefaultLogger.Log("This is a log message")
+
+	// Create a custom logger
+	var buf bytes.Buffer
+	customLogger := logger.Logger{Writer: &buf}
+	customLogger.Log("Log to buffer")
+}
+```
+
+## Package Documentation
+
+You can view the full documentation on **pkg.go.dev**:
+
+🔗 [https://pkg.go.dev/github.com/jaskaran27177/go-log](https://pkg.go.dev/github.com/jaskaran27177/go-log)
+
+## Features
+
+- Simple logging interface
+- Supports any `io.Writer`
+- Default logger writes to `os.Stdout`
+
+## License
+
+MIT License
--- log.go
+++ log.go
@@ -1,3 +1,7 @@
+// Package logger provides a simple logging utility with a customizable writer.
+//
+// This package is designed to support basic logging by either writing to standard output
+// or any `io.Writer` provided by the user.
 package logger
 
 import (
@@ -5,15 +9,32 @@ import (
 	"io"
 	"os"
 )
+
+// Logger represents a simple logger that writes logs to a specified io.Writer.
 type Logger struct {
-    writer io.Writer
+	writer io.Writer
 }
 
-func(reciever Logger) Log(a... any){
+// Log writes a log message to the configured writer.
+//
+// It accepts any number of arguments, formats them using fmt.Fprintln, and writes the
+// result to the logger's output writer.
+//
+// Example usage:
+//
+//     logger.DefaultLogger.Log("Hello", "world")
+//
+// This will print: `Hello world` to the default writer (os.Stdout).
+func (reciever Logger) Log(a ...any) {
 	fmt.Fprintln(reciever.writer, a...)
 }
+
+// DefaultLogger is the default logger instance that writes to os.Stdout.
+//
+// Users can either use this directly or create their own Logger with a custom writer.
 var DefaultLogger Logger
+
+// init initializes the DefaultLogger to write to standard output (os.Stdout).
 func init() {
 	DefaultLogger.writer = os.Stdout
-
 }
